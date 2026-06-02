b29c7be11b46d8a631ef766372c63568b5820ede
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-06-03 02:02:42 +0530

    fix running on linux

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,22 @@ import 'package:calculator_flutter_app/src/rust/frb_generated.dart';
 import 'package:calculator_flutter_app/app.dart';
 
 import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  await RustLib.init();
+  ExternalLibrary? externalLibrary;
+  if (Platform.isLinux) {
+    final executable = Platform.resolvedExecutable;
+    final libPath = path.join(path.dirname(executable), 'lib', 'librust_lib_calculator_flutter_app.so');
+    if (File(libPath).existsSync()) {
+      externalLibrary = ExternalLibrary.open(libPath);
+    }
+  }
+  await RustLib.init(externalLibrary: externalLibrary);
   
   runApp(
     const ProviderScope(
